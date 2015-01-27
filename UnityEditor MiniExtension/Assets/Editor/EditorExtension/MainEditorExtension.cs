02644e19bfc09363c84e3979a7e3a5691086f28d
lPinchol <pinchodev91@gmail.com>
2015-01-27 23:53:34 +0100

    Add Funcionality
    Add clear PlayerPref and EditorPref funcionality.

---
+++ UnityEditor MiniExtension/Assets/Editor/EditorExtension/MainEditorExtension.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class MainEditorExtension
+{
+    [MenuItem(EditorStrings.Setup.StringDeletePrefs,false,0)]
+    private static void MenuDeletePrefs()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+
+    [MenuItem(EditorStrings.Setup.StringDeleteEditorPrefs, false, 1)]
+    private static void MenuDeleteEditorPrefs()
+    {
+        EditorPrefs.DeleteAll();
+    }
+}
+
+public class EditorStrings
+{
+    public class Setup
+    {
+        public const string StringDeletePrefs                               = "Tools/PlayerPrefs/Clear PlayerPrefs";
+        public const string StringDeleteEditorPrefs                         = "Tools/PlayerPrefs/Clear EditorPrefs";
+    }
+}
+
+
